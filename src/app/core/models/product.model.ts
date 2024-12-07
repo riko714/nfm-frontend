77111514de91df0f738f176771656d3a8372510c
Raf <riko714@gmail.com>
2024-12-07 16:47:43 -0500

    update

---
+++ src/app/core/models/product.model.ts
@@ -0,0 +1,11 @@
+export interface Product {
+    PLU_ID: string;
+    UPC_Code: string;
+    Brand?: string;
+    DESC_EN1?: string;
+    Regular_Price?: number;
+    SalePrice?: number;
+    On_hand?: number;
+    // Add as many fields as needed from the entity
+  }
+  
